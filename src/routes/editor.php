d339e4dd74a5998ca7ccfcf14ed4942b89f3f9f5
Kaviyarasu <40977904+kaviphp@users.noreply.github.com>
2023-04-13 15:44:03 +0530

    user verification

--- src/routes/editor.php
+++ src/routes/editor.php
@@ -5,7 +5,8 @@ use OneClx\SiteBuilder\Http\Controllers\SiteEditorController;
 
 
 Route::middleware(['web', 'csrf', 'verified', 'role:pcx'])->controller(SiteEditorController::class)->group(function () {
-    Route::get('{business}', 'editor')->name('editor');
+    Route::get('{business}', 'editor')->name('editor')
+        ->middleware('editor');
     Route::get('business', 'business')->name('index');
     Route::post('upload', 'upload')->name('upload');
     Route::post('save', 'save')->name('save');
